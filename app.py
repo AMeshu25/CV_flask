c7cc47c1460058be4617b6d7e265b117f90cbf63
A.Meshu25 <meshurer@post.bgu.ac.il>
2021-12-28 21:09:23 +0200

    assingmet9

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, request,session,redirect
+import mysql.connector
 app = Flask(__name__)
+app.secret_key='123456'
+users = {'user1': {'name': 'Amir', 'email': 'AM@gmail.com', 'nickname': 'Miri', 'b-day': '25/05/1995 '},
+         'user2': {'name': 'Or', 'email': 'Orda@gmail.com', 'nickname': 'Orcha', 'b-day': '20/08/1995'},
+         'user3': {'name': 'Ron', 'email': 'sad@gmail.com', 'nickname': 'Ponia ', 'b-day': '14/04/1995'},
+         'user4': {'name': 'Ofek', 'email': 'Ope@gmail.com','nickname': 'Puki' ,'b-day': '20/10/1996' },
+         'user5': {'name': 'Shir', 'email': 'shir@gmail.com', 'nickname': 'Chir', 'b-day': '13/8/1993'} }
 
 
 @app.route('/')
@@ -17,7 +23,39 @@ def Second_page():  # put application's code here
 def forms_page():  # put application's code here
     return render_template('forms.html')
 
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    if request.method == 'GET':
+        # someone made a search
+        if 'searchName' in request.args:
+            # request.args for GET method
+            search = request.args['searchName']
+            # empty search - section 4
+            if search == '':  # need to return all the users
+                return render_template('assignment9.html', allUsers=users)
+            # not empty search - section 3
+            else:
+                user = ' '
+                for userID, userInfo in users.items():
+                    if search == userInfo['name']:
+                        user = userInfo
+                        break
+                return render_template('assignment9.html', foundUser=user)
+        # no one made a search
+        return render_template('assignment9.html')
+    elif request.method == 'POST':
+        # request.form for POST method
+        if 'username' in request.form:  # at least username was enterd
+            username = request.form['username']
+            password = request.form['password']
+            session['username'] = username
+            return render_template('assignment9.html')
+        return render_template('assignment9.html')
 
+@app.route('/logout')
+def logout_func():
+    session['username'] = ''
+    return redirect(url_for('assignment9'))
 
 @app.route('/assignment8')
 def something_page():  # put application's code here
